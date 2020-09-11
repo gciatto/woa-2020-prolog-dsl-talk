c234f7c4d49c0df80ea277d4eb7598ca17605ecc
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-09-11 18:52:03 +0200

    writing contribution of the paper

--- code/SwiFliExample.java
+++ code/SwiFliExample.java
@@ -1,9 +1,7 @@
-Query query = new Query( 
-    "parent", 
-    new Term[] { 
-      new Atom("adam"), 
-      new Variable("X") 
-    } 
-  );
+Query query = new Query("parent", new Term[] {
+      new Atom("adam"),
+      new Variable("X")
+    }
+  );  // ?- parent(adam, X).
 Map<String,Term> solution = query.oneSolution();
 System.out.println("The child of Adam is " + solution.get("X"));
