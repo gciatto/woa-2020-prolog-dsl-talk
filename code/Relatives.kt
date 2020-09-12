bbd884192a807963ff5bc6b08b3101550ce42004
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2020-09-12 12:34:29 +0200

    some slides

--- code/Relatives.kt
+++ code/Relatives.kt
@@ -1,20 +1,21 @@
 prolog {
   staticKb(
     rule {
-      "ancestor"("X", "Y") `if` "parent"("X", "Y")
+      "ancestor"(X, Y) `if` "parent"(X, Y)        // ancestor(X, Y) :- parent(X, Y).
     },
     rule {
-      "ancestor"("X", "Y") `if` (
-        "parent"("X", "Z") and "ancestor"("Z", "Y")
+      "ancestor"(X, Y) `if` (                     // ancestor(X, Y) :-
+        "parent"(X, Z) and "ancestor"(Z, Y)       //     parent(X, Z), ancestor(Z, Y).
       )
     },
-    fact { "parent"("abraham", "isaac") },
-    fact { "parent"("isaac", "jacob") },
-    fact { "parent"("jacob", "joseph") }
+    fact { "parent"("abraham", "isaac") },        // parent(abraham, isaac).
+    fact { "parent"("isaac", "jacob") },          // parent(isaac, jacob).
+    fact { "parent"("jacob", "joseph") }          // parent(jacob, joseph).
   )
 
-  for (sol in solve("ancestor"("abraham", "X")))
-    if (sol is Solution.Yes)
-      println(sol.substitution["X"])
+  for (sol in solve("ancestor"("abraham", D)))    // ?- ancestor(abraham, D),
+    if (sol is Solution.Yes)                      //        write(D), nl.
+      println(sol.substitution[D])
 }
+
 
